d5f260310a8b19b21e5448531ccbbe88c96982bc
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-02 19:38:40 +0000

    Update password.js

--- password.js
+++ password.js
@@ -1,31 +1,43 @@
 console.log("STARTED")
-import * as $ from './jquery.js';
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  for(let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) === ' ') {
+    while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
-    if (c.indexOf(name) === 0) {
+    if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
   }
   return "";
 }
 
-if(getCookie("passwordstate")!=="pass"){
-  var password = prompt("Please enter the password.");
-  $.getJSON(`https://networkcalc.com/api/encoder/${password}?encoding=base64`,
+function checkCookie() {
+  let pass = getCookie("passwordstate");
+  if (pass != "") {
+    
+  } else {
+     var password = prompt("Please enter the password.");
+  		$.getJSON(`https://networkcalc.com/api/encoder/${password}?encoding=base64`,
         function (data) {
             var dec = data.encoded;
             if(dec==="MjQxMjE5ODg="){
               console.log("PASS!")
-              document.cookie = "passwordstate=passed";
+              setCookie("passwordstate","passed",365)
               location.reload;
             }else{
               console.log("FAIL!")
               window.location.replace('https://fixcraftvpn.onrender.com/forbidden')
-            };})}
+            };})
+     }
+  }
