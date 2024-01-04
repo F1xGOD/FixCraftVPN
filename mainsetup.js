f64541c6cc35f6252015f3dd2b9f0f8931f61cd4
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-04 17:34:50 -0500

    Update mainsetup.js

--- mainsetup.js
+++ mainsetup.js
@@ -1,7 +1,7 @@
 import {data3} from "/setup.js";
 var actdata=data3.accounts;
-				let i = 0;
-				while (i < actdata.length) {
-    					console.log(actdata[i]);
-    					i++;
-					}
+let i = 0;
+while (i < actdata.length) {
+console.log(actdata[i]);
+i++;
+}
