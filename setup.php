83407be733b1377ad12befeecdc0d90aa255cb59
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-04 19:42:06 +0000

    Delete setup.php

--- setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = (int)$_REQUEST['id'];
-header('Content-Type: application/json');
-if ($id % 2 == 0) echo('{"id" : "' . $id . '", "success" : "1"}');
-else echo('{"id" : "' . $id . '", "success" : "0", "error" : "Could not delete"}');
-?>
