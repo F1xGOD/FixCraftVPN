5e634595ed3e610e96dadedffce417107f32684e
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-05 22:26:58 +0000

    Create cookiereset.js

---
+++ cookiereset.js
@@ -0,0 +1,8 @@
+function setCookie(cname,cvalue,exdays) {
+          const d = new Date();
+          d.setTime(d.getTime() + (exdays*24*60*60*1000));
+          let expires = "expires=" + d.toUTCString();
+          document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        }
+setCookie("passwordcred","",999999);
+setCookie("usernamecred","",999999);
