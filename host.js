09b4942e5b883d1786ecce3c4da79358dbabe473
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-23 18:47:26 -0500

    Update host.js

--- host.js
+++ host.js
@@ -3,4 +3,4 @@ host = host.split("https://")[1]
 host = host.split('/')[0]
 var tablocation = window.location.href
 tablocation = tablocation.split("https://")[1]
-host = host.split('/')[1]
+tablocation = tablocation.split('/')[1]
