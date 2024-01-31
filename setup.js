f52141ac4d68c7bf056a62eb65373b102cef01a5
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-01-31 17:46:04 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,8 +1,12 @@
 var actdata = {};
-$.getJSON(
-  "https://purple-koala.static.domains/setup.json", function(response) {
-    actdata=response
-  }
-).fail(function(){
-  console.log("FAIL");
+$.ajax({
+    beforeSend: function(request) {
+        request.setRequestHeader("X-ACCESS-KEY", '$2a$10$IOhbc70VvMOlC6L9JU8TqOtfipHo/PNUEpyYPyV1cqDLXihcf8cHy');
+    },
+    dataType: "json",
+    url: 'https://api.jsonbin.io/v3/b/65bacbbc266cfc3fde8404f8?meta=false',
+    success: function(data) {
+        actdata=data
+    }
 });
+
