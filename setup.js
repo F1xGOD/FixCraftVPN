92c31593716281ffa67c94ce390f97fd42c14d16
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-06 14:30:54 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,12 +1,7 @@
 var actdata = {};
-$.ajax({
-    beforeSend: function(request) {
-        request.setRequestHeader("X-ACCESS-KEY", '$2a$10$Ld/HajVSMMFXtlZwqCJgJ.da7e2YfyNnuTitGgc7BNnH.qXVipeSS');
-    },
-    dataType: "json",
-    url: 'https://api.jsonbin.io/v3/b/65bd1fa6266cfc3fde84da0f?meta=false',
-    success: function(data) {
-        actdata=data
-    }
-});
 
+
+$.getJSON(`https://65c242abf7e6ea59682b0378.mockapi.io/data/data/0`,
+            	function (data) {
+actdata=data.json
+})
