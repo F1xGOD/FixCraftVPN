bc42fdd3667d479b8180b53ec7241ba2d73d6e5c
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-06 21:12:07 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -156,12 +156,11 @@
 //NAH BRO LAST CHANCE
 
 var actdata = {};
-var env = window._env_
 
 //I WILL FIND YOU....
 
-$.getJSON(`${env.datasource}`,
+$.getJSON(`https://65c242abf7e6ea59682b0378.mockapi.io/data/data/0`,
             	function (data) {
 actdata=data.json
-windows._env_.datasource = "FUCK YOU!"
+
 })
