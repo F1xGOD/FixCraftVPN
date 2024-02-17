8f918916a9dc00cf970d6feb3caced9f0e2c4d6e
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-17 17:30:39 -0500

    2

--- password.js
+++ password.js
@@ -1,5 +1,15 @@
 
-import {actdata} from "/setup.js";
+import { initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+const firebaseConfig = { apiKey: "AIzaSyAQ89agVS02dwVK9-yWwpZOvMkQWLiKcEM", authDomain: "fixcraft-vpn.firebaseapp.com", databaseURL: "https://fixcraft-vpn-default-rtdb.firebaseio.com", projectId: "fixcraft-vpn", storageBucket: "fixcraft-vpn.appspot.com", messagingSenderId: "811886239981", appId: "1:811886239981:web:9e43da7b31be5f7fb1ace4", measurementId: "G-CTR9TEET9E" };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+var actdata = {};
+const infodat = ref(db, 'data');
+onValue(infodat, (snapshot) => {
+    const data = snapshot.val();
+      actdata=data.data
+  })
 function warntheuser(){
 console.clear();
 console.log('%cWARNING!', 'color: #ff0000; font-size: 36px; font-weight: bold');
