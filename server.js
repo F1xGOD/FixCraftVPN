f9a7e0a2604c4420407021c384a0aa37b41c6614
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-24 13:27:41 -0500

    dwfw

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require('http');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World!');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
