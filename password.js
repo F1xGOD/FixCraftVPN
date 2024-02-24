eb74ca976743839be0d93b7df385cbc25e891776
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-24 11:41:37 -0500

    ffsd

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 
-import { initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { initializeApp, firebase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { getMessaging, onMessage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js";
 import "/platform.js";
