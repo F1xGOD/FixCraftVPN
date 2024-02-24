510fc53ccd13b0720ce1bbde31b33f22c9cee5f2
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-24 12:44:05 -0500

    sfds

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "/bare.json",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
