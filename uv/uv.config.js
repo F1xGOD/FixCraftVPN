c273052ea8f9764ba955b513b532379862f49417
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-24 20:22:57 -0500

    fwefw

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://cerryx.net/bare/",
+  bare: "https://anatomical.design/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
