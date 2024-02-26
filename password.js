409c51e1fc5aeebb0dd54f0b0c62c85b5897a05f
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-02-26 12:09:05 -0500

    Update password.js

--- password.js
+++ password.js
@@ -332,7 +332,8 @@ if(tablocation != "login"){
 			window.location.replace(`${host}/forbidden`)
 		}
 	  }
-	  if(username == "" && password == ""){if(shd==true){window.location.replace(`${host}/login`)}}else{
+	console.log(username == "" && password == ""&&shd==true)	 
+	if(username == "" && password == ""){if(shd==true){window.location.replace(`${host}/login`)}}else{
 				var data4=btoa(username)
 				var data5=btoa(password)
 				Object.freeze(data4);
