543ac5742c2aa840af7ff216de979db1c49630cd
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-05-31 21:03:18 -0700

    2

--- settings.js
+++ settings.js
@@ -8,7 +8,12 @@
           websiteTitleElem.textContent = title;
         }
       }
-  
+      function setCookie(cname,cvalue,exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
       const icon = localStorage.getItem('websiteIcon');
       if (icon) {
         let favicon = document.getElementById('favicon');
