b0e816405afa15526c980ef709ca5646b09ff208
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-06-01 19:49:59 -0700

    2

--- default.js
+++ default.js
@@ -1,2 +1,2 @@
 var defjson = {"favicon":"/FixCraft.png","title":"FixCraftV2"}
-document.title = document.title.replaceAll(document.title.split(" | ")[0],defjson.title)
+document.title = document.title.replaceAll("FixCraft VPN",defjson.title).replaceAll("FixCraftVPN",defjson.title)
