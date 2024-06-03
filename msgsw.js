058f771e1d44f2f6c06e98bf796906333eb8236f
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-06-03 10:57:48 -0700

    12

--- msgsw.js
+++ msgsw.js
@@ -1,1 +1,10 @@
-
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const title = 'Push Codelab';
+    const options = {
+      body: 'Yay it works.',
+      icon: '/FixCraft.png',
+      badge: '/FixCraft.png'
+    };})
