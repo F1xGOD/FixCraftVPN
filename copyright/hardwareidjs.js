c3460715912bc0604226da7c6e6163f744a971fc
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-06-04 22:36:44 -0700

    31

---
+++ copyright/hardwareidjs.js
@@ -0,0 +1,8 @@
+/**
+ * HardwareIDJS v1.0.0 - Copyright (c) FixCraft, Inc, 2024 (https://fixcraft.org)
+ *
+ * Licensed under Business Source License 1.1 https://mariadb.com/bsl11/
+ * Licensor: FixCraft, Inc.
+ * Additional Use Grant: None
+ * Copyright 2020-Present FixCraft, Inc.
+ */
