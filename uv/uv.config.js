6944a8e0a3dbb4a56007c3a5d7f1c24b598c0c10
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-06-19 18:05:15 -0700

    wqdrtyui

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,4 +1,4 @@
 // This file overwrites the stock UV config.js
 
-self.__uv$config = {"prefix":"/uv/service/","bare":"https://bare.fixcraft.org/","encodeUrl":Ultraviolet.codec.xor.encode,"decodeUrl":Ultraviolet.codec.xor.decode,"handler":"/uv/uv.handler.js","client":"/uv/uv.client.js","bundle":"/uv/uv.bundle.js","config":"/uv/uv.config.js","sw":"/uv/uv.sw.js"}
+self.__uv$config = {"prefix":"/uv/service/","bare":"http://localhost:10000/","encodeUrl":Ultraviolet.codec.xor.encode,"decodeUrl":Ultraviolet.codec.xor.decode,"handler":"/uv/uv.handler.js","client":"/uv/uv.client.js","bundle":"/uv/uv.bundle.js","config":"/uv/uv.config.js","sw":"/uv/uv.sw.js"}
 
