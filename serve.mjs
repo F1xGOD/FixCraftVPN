c82f0814913b36291af934b766fe48c1e6a248d2
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-08-05 18:53:59 -0700

    ftgyhy

--- serve.mjs
+++ serve.mjs
@@ -1,6 +1,8 @@
 import mime from 'mime'
-import { readFileSync } from 'fs'
-import { getHWID } from "./public/copyright/hardwareidjs.js"
+import { readFileSync } from 'fs';
+import { get } from 'http';
+
+
 
 var defaults = {
   indexfile: true,
@@ -8,9 +10,6 @@ var defaults = {
   '403': null,
   'index': null
 }
-const hwidban = JSON.parse(fs.readFileSync('ban.json'));
-var hwid="000x000"
-getHWID().then((result)=>{hwid=result})
 
 export function Serve(path, config, cors) {
   var config = Object.assign(defaults, config)
@@ -21,14 +20,6 @@ export function Serve(path, config, cors) {
       if (req.url.split('?')[0].split('#')[0]=='/'&&config.indexfile==true) {url+='index.html'}
       try {readFileSync(url)} catch {try {readFileSync(url+=".html")} catch { return res.end('Not Found') }}
       if (req.url.split('?')[0].split('#')[0]=='/'&&config.indexfile==true) {if (config.index) return config.index(req, res, readFileSync(url), type)}
-      for(var oio = 0; oio<hwidban.length; oio++){
-        if(hwidban[oio]!="NONE"){
-        if(hwidban[oio].hwid==hwid&&hwidban[oio].type=="soft"){
-          return res.writeHead(200, {'content-type':type, 'access-control-allow-origin':cors?'*':request.headers['host']}).end(readFileSync("ban.html"))
-        }else if(hwidban[oio].hwid==hwid&&hwidban[oio].type=="hard"){
-          return res.end('Not Found')
-        }}
-      }
       return res.writeHead(200, {'content-type':type, 'access-control-allow-origin':cors?'*':request.headers['host']}).end(readFileSync(url))
     } else {
       throw new Error('Request Object: Expected [object Request], got undefined')
