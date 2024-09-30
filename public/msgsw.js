c986d5e955bfae7f1c24b2025a49a69ca0d86097
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-09-29 22:33:39 -0700

    h122ws
    uh

--- public/msgsw.js
+++ public/msgsw.js
@@ -1,13 +1,21 @@
-self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-  
-    const title = 'Push Codelab';
-    const options = {
-      body: 'Yay it works.',
-      icon: 'images/icon.png',
-      badge: 'images/badge.png'
-    };
-  
-    event.waitUntil(self.registration.showNotification(title, options));
-  });
+import { initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+const firebaseConfig = { apiKey: "AIzaSyAQ89agVS02dwVK9-yWwpZOvMkQWLiKcEM", authDomain: "fixcraft-vpn.firebaseapp.com", databaseURL: "https://fixcraft-vpn-default-rtdb.firebaseio.com", projectId: "fixcraft-vpn", storageBucket: "fixcraft-vpn.appspot.com", messagingSenderId: "811886239981", appId: "1:811886239981:web:9e43da7b31be5f7fb1ace4", measurementId: "G-CTR9TEET9E" };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const infodat = ref(db, 'data');
+onValue(infodat, (snapshot) => {
+    var data = snapshot.val();
+  const data = {"title":"FixCraftV2 - Chat"}
+
+  const options = {
+  tag: "Chat",
+  body: snd+" - "+msg,
+  icon: "FixCraft.png",
+  image: "FixCraft.png",
+  vibrate: 500,
+}
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );})
